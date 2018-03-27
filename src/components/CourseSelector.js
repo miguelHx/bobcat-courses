b792302bfacd4ca7c802e99a76c4a7a3e37fbf9d
Miguel Hernandez <mhernandez401405@gmail.com>
2018-03-27 14:40:54 -0700

    remove redundant state and data fetch, place handlers from parent component

--- src/components/CourseSelector.js
+++ src/components/CourseSelector.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import courseJSON from './../../data/courses_sample_data.json';
 import AddCourse from './AddCourse';
 import Courses from './Courses';
 
@@ -11,46 +10,23 @@ import Courses from './Courses';
 class CourseSelector extends React.Component {
   state = {
     // default state values
-    selectedDepartment: '',
-    selectedCourse: '',
     courses: [], // maybe store array of course objects in here? will be easier for algorithm
   };
 
   componentDidMount() {
-    //initialize data
-    console.log("comp. mounted.  Want to get json data and store either locally (for development) or through an AJAX request (production).");
-    window.jsonData = courseJSON;
-    const departments = Object.keys(window.jsonData);
-    const courses = Object.keys(window.jsonData[departments[0]]);
 
-    const courseData = window.jsonData[departments[0]][courses[0]];
-    console.log("Course data:");
-    console.log(courseData);
-
-    //const jsonObj = JSON.parse(courseJSON);
-    console.log("JSON file: ");
-    console.log(window.jsonData);
-    //console.log(jsonObj);
-    console.log("Printing out main keys from JSON file:");
-    console.log(departments);
-    console.log("printing out courses from Anthropology");
-    console.log(courses);
-
-
-    this.setState(() => ({ selectedDepartment: departments[0], selectedCourse: courses[0] }));
-    // print courses from Anthropology
   }
 
   handleDeptDropdown = (event) => {
-    // want to update selectedCourse state as well
+    // want to update selectedCourse state as well by calling handler from app root
     const dept = event.target.value;
     const course = Object.keys(window.jsonData[dept])[0];
-    this.setState(() => ({ selectedDepartment: dept, selectedCourse: course}));
+    this.props.handleDeptDropdown(dept);
   };
 
   handleCourseDropdown = (event) => {
     const course = event.target.value;
-    this.setState(() => ({ selectedCourse: course }));
+    this.props.handleCourseDropdown(course);
   };
 
   handleDeleteCourses = () => {
@@ -80,6 +56,7 @@ class CourseSelector extends React.Component {
     }));
   };
 
+  // move generate schedules into root directory
   generateSchedules = () => {
     console.log("Want to first check size of courses array.");
     console.log("Take courses, use courses array to get information from JSON, run algorithm.");
@@ -90,13 +67,11 @@ class CourseSelector extends React.Component {
   render() {
     return (
       <div>
-        <p>Selected Department: {this.state.selectedDepartment}</p>
-        <p>Selected Course: {this.state.selectedCourse}</p>
         <AddCourse
           handleAddCourse={this.handleAddCourse}
           handleDeptDropdown={this.handleDeptDropdown}
           handleCourseDropdown={this.handleCourseDropdown}
-          selectedDepartment={this.state.selectedDepartment}
+          selectedDepartment={this.props.selectedDepartment}
         />
         {
           this.state.courses.length === 0 &&
