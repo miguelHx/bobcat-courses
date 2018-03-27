781fe310248c908bfe5927808e08fd73e9701262
Miguel Hernandez <mhernandez401405@gmail.com>
2018-03-27 16:59:05 -0700

    Got semantic css to work with scss, installed 3 more dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      },
+      {
         loader: 'babel-loader',
         test: /\.js$/,
         exclude: /node_modules/
