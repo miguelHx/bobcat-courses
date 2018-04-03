f73bc987f0fa3316ce01ab0c8c14a3510c42a67c
Miguel Hernandez <mhernandez401405@gmail.com>
2018-04-03 12:29:43 -0700

    add logo img to header, use flexbox for layout of container

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import logo from '../../assets/bobcat-logo.png';
 
 const Header = (props) => {
   return (
     <div className="header">
       <div className="container">
-        <h1 className="header__title">{props.title}</h1>
-        <h2 className="header__subtitle">{props.subtitle}</h2>
+        <img src={logo} />
+        <div className="header__text__wrapper">
+          <h1 className="header__title">{props.title}</h1>
+          <h2 className="header__subtitle">{props.subtitle}</h2>
+        </div>
       </div>
 
     </div>
