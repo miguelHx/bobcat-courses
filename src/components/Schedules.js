5fb57def3b945f1862bcbd32837f2e515e9abf30
Miguel Hernandez <mhernandez401405@gmail.com>
2018-04-07 13:07:46 -0700

    setup sample static calendar, design similar to Apple Calendar

--- src/components/Schedules.js
+++ src/components/Schedules.js
@@ -3,9 +3,96 @@ import ReactDOM from 'react-dom';
 
 class Schedules extends React.Component {
   render() {
+    if (!this.props.validSchedules) {
+      return <div className="schedules__container">Loading...</div>;
+    }
     return (
       <div className="schedules__container">
-        <h1>Calendars will display here when valid schedules is > 0 and then selectedCourse is undefined</h1>
+        <div className="calendar__top-header-divider"></div>
+        <div className="calendar-header__row">
+          <div className="time-header"></div>
+          <div className="calendar-header__row-title">Mon</div>
+          <div className="calendar-header__row-title">Tue</div>
+          <div className="calendar-header__row-title">Wed</div>
+          <div className="calendar-header__row-title">Thu</div>
+          <div className="calendar-header__row-title">Fri</div>
+        </div>
+        <div className="weekly-cal-view__container">
+          <div className="weekly-cal-view__time-col">
+            <div className="time">7</div>
+            <div className="time">8</div>
+            <div className="time">9</div>
+            <div className="time">10</div>
+            <div className="time">11</div>
+            <div className="time">Noon</div>
+            <div className="time">1</div>
+            <div className="time">2</div>
+            <div className="time">3</div>
+            <div className="time">4</div>
+          </div>
+
+          <div className="weekly-cal-view__monday-col">
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+          </div>
+          <div className="weekly-cal-view__tuesday-col">
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+          </div>
+          <div className="weekly-cal-view__wednesday-col">
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+          </div>
+          <div className="weekly-cal-view__thursday-col">
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+          </div>
+          <div className="weekly-cal-view__friday-col">
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+            <div className="weekly-hour-slot"></div>
+          </div>
+        </div>
+
       </div>
     );
   }
