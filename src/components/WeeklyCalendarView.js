4e33652ea1385e10847942b707d20ae3e02cbb6c
Miguel Hernandez <mhernandez401405@gmail.com>
2018-04-07 15:57:01 -0700

    weekly calendar renders time rows based on min/max time for current schedule

--- src/components/WeeklyCalendarView.js
+++ src/components/WeeklyCalendarView.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+let id = 0;
+
 // maybe add testing for this funcion to handle all types of errors
 const convert24to12HourFormat = (time24) => {
   // expecting numbers such as 7, 8, 9, 10, 11, 12, 13, 14 etc.
@@ -13,186 +15,72 @@ const convert24to12HourFormat = (time24) => {
 
 export default class WeeklyCalendarView extends React.Component {
 
-  renderTimes = (minTime, maxTime) => {
-    // min and max will come as 24-hour time.
+  renderWeekColumnRows = (numRowsToRender) => {
+    let times = [];
+    for (let i = 0; i <= numRowsToRender; i++) {
+      times.push(i);
+    }
 
-    const start = (Math.floor(minTime/100)*100)/100 - 1;
-    const end = (Math.ceil(maxTime/100)*100)/100 + 1;
+    return (
+      times.map((time) => {
+        return (
+          <div key={id++} className="weekly-hour-slot">
+            <div className="weekly-hour__30min-dotted-line"></div>
+          </div>
+        );
+      })
+    );
+  };
 
+  renderTimes = (start, end) => {
     console.log(`Earliest time: ${start}, Latest time: ${end}`);
 
+    let times = [];
+    for (let i = start; i <= end; i++) {
+      times.push(i);
+    }
 
-
-
-
-  }
+    return (
+      times.map((time) => {
+        let currTime = time;
+        if (currTime === 12) {
+          currTime = 'Noon';
+        }
+        else {
+          currTime = convert24to12HourFormat(time);
+        }
+        return (
+          <div key={currTime} className="time">{currTime}</div>
+        );
+      })
+    );
+  };
 
   render() {
+    console.log("[WEEKLY VIEW] current valid schedule: ", this.props.currSchedule);
+    const earliest = this.props.currSchedule['info']['earliest'];
+    const latest = this.props.currSchedule['info']['latest'];
+    const start = (Math.floor(earliest/100)*100)/100 - 1;
+    const end = (Math.ceil(latest/100)*100)/100 + 1;
     return (
       <div className="weekly-cal-view__container">
         <div className="weekly-cal-view__time-col">
-          { this.renderTimes(730, 1030) }
+          { this.renderTimes(start, end) }
         </div>
-
         <div className="weekly-cal-view__monday-col">
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
+          { this.renderWeekColumnRows(end-start) }
         </div>
         <div className="weekly-cal-view__tuesday-col">
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
+          { this.renderWeekColumnRows(end-start) }
         </div>
         <div className="weekly-cal-view__wednesday-col">
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
+          { this.renderWeekColumnRows(end-start) }
         </div>
         <div className="weekly-cal-view__thursday-col">
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
+          { this.renderWeekColumnRows(end-start) }
         </div>
         <div className="weekly-cal-view__friday-col">
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
-          <div className="weekly-hour-slot">
-            <div className="weekly-hour__30min-dotted-line"></div>
-          </div>
+          { this.renderWeekColumnRows(end-start) }
         </div>
       </div>
     );
