0c223ed793aec1d2e9c2e3d4794b750e13df4a75
Miguel Hernandez <mhernandez401405@gmail.com>
2018-04-07 17:25:06 -0700

    test out static event on calendar, setup methods for placing all sections onto calendar
    Will need to add CSS dynamically for each section event.  I can do this with javascript.
    Next step is to figure out offset formula for section placement.  10px + (timeofsection - earliest time)*heightof1hourblockinpixels
    or something like that.  Then, the height of the section will depend on its time length.

--- src/components/WeeklyCalendarView.js
+++ src/components/WeeklyCalendarView.js
@@ -2,6 +2,20 @@ import React from 'react';
 
 let id = 0;
 
+const extractSectionsFromSchedule = (currSchedule) => {
+  const coursesList = Object.keys(currSchedule['schedule']); // ['ANTH-1', 'MATH-32', etc.]
+  let sectionsList = [];
+  for (let i = 0; i < coursesList.length; i++) {
+    let currCourse = coursesList[i];
+    let currSections = currSchedule['schedule'][currCourse];
+    let currSectionKeys = Object.keys(currSections); // ['DISC', 'LAB', ...]
+    for (let j = 0; j < currSectionKeys.length; j++) {
+      sectionsList.push(currSections[currSectionKeys[j]]);
+    }
+  }
+  return sectionsList;
+};
+
 // maybe add testing for this funcion to handle all types of errors
 const convert24to12HourFormat = (time24) => {
   // expecting numbers such as 7, 8, 9, 10, 11, 12, 13, 14 etc.
@@ -11,9 +25,36 @@ const convert24to12HourFormat = (time24) => {
   else {
     return time24 - 12;
   }
-}
+};
 
 export default class WeeklyCalendarView extends React.Component {
+
+  componentDidMount() {
+    const currSchedule = this.props.currSchedule;
+    const sectionsList = extractSectionsFromSchedule(currSchedule);
+
+    const earliest = currSchedule['info']['earliest'];
+    const start = (Math.floor(earliest/100)*100)/100 - 1;
+
+    this.placeSectionsIntoCalendar(start, sectionsList);
+  }
+
+  componentDidUpdate() {
+    const currSchedule = this.props.currSchedule;
+    const sectionsList = extractSectionsFromSchedule(currSchedule);
+
+    const earliest = currSchedule['info']['earliest'];
+    const start = (Math.floor(earliest/100)*100)/100 - 1;
+
+    this.placeSectionsIntoCalendar(start, sectionsList);
+
+  }
+
+  placeSectionsIntoCalendar = (startingHour, allSections) => {
+    console.log("starting hour: ", startingHour);
+    console.log("all sections: ", allSections);
+    
+  };
 
   renderWeekColumnRows = (numRowsToRender) => {
     let times = [];
@@ -67,19 +108,29 @@ export default class WeeklyCalendarView extends React.Component {
         <div className="weekly-cal-view__time-col">
           { this.renderTimes(start, end) }
         </div>
-        <div className="weekly-cal-view__monday-col">
+        <div className="weekly-cal-view__monday-col" ref="monday">
+          <div className="event">
+            <div className="text">
+              <div className="title">
+                CSE 111 LAB 02L
+              </div>
+              <div className="location">
+                SCIENG 100
+              </div>
+            </div>
+          </div>
           { this.renderWeekColumnRows(end-start) }
         </div>
-        <div className="weekly-cal-view__tuesday-col">
+        <div className="weekly-cal-view__tuesday-col" ref="tuesday">
           { this.renderWeekColumnRows(end-start) }
         </div>
-        <div className="weekly-cal-view__wednesday-col">
+        <div className="weekly-cal-view__wednesday-col" ref="wednesday">
           { this.renderWeekColumnRows(end-start) }
         </div>
-        <div className="weekly-cal-view__thursday-col">
+        <div className="weekly-cal-view__thursday-col" ref="thursday">
           { this.renderWeekColumnRows(end-start) }
         </div>
-        <div className="weekly-cal-view__friday-col">
+        <div className="weekly-cal-view__friday-col" ref="friday">
           { this.renderWeekColumnRows(end-start) }
         </div>
       </div>
