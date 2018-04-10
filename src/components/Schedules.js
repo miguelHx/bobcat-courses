f505a0e8bdd4a295c7cd067420c48bd9bdc5dda5
Miguel Hernandez <mhernandez401405@gmail.com>
2018-04-09 21:03:12 -0700

    schedule weekly view now works properly!! move day of week sections to schedules comp. details:
    So we moved a bunch of stuff from the WeeklyCalendarView to the Schedules.js. reason being,
    it was much easier to update the state of day of week sections arrays when the arrow button
    was clicked rather than checking if the index changed in the child component.
    Next, we gotta style up, take into account mobile views, and filter unselected sections. :)

--- src/components/Schedules.js
+++ src/components/Schedules.js
@@ -1,40 +1,224 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import moment from 'moment';
 import { Button, Icon } from 'semantic-ui-react';
+import { extractSectionsFromSchedule } from './../lib/WeeklyCalendarUtils';
+import { convertTimeStringTo24 } from './../lib/WeeklyCalendarUtils';
 import WeeklyCalendarHeader from './WeeklyCalendarHeader';
 import WeeklyCalendarView from './WeeklyCalendarView';
+
+let id = 1000;
 
 class Schedules extends React.Component {
   state = {
     currIndex: 0,
+    mondaySections: [],
+    tuesdaySections: [],
+    wednesdaySections: [],
+    thursdaySections: [],
+    fridaySections: [],
+  };
+
+  componentDidMount() {
+    // initial rendering of sections onto page
+    console.log("COMPONENT DID MOUNT");
+    const currIdx = this.state.currIndex;
+    const currSchedule = this.props.validSchedules[currIdx];
+    const sectionsList = extractSectionsFromSchedule(currSchedule);
+
+    const earliest = currSchedule['info']['earliest'];
+    const start = (Math.floor(earliest/100)*100)/100 - 1;
+    this.placeSectionsIntoCalendar(start, sectionsList, 0);
+  }
+
+
+  placeSectionsIntoCalendar = (startingHour, allSections, indexUpdate) => {
+    console.log("INSIDE PLACE SECTIONS INTO CALENDAR");
+
+    let startHr = (startingHour * 100).toString(10); // will be used for moment js
+
+    let hr;
+    if (startingHour < 10) {
+       hr = `0${startHr.substring(0, 1)}`;
+    }
+    else {
+      hr = startHr.substring(0, 2);
+    }
+    const min = startHr.slice(-2);
+    startHr = moment(`${hr}:${min}`, 'HH:mm');
+
+    console.log("starting hour: ", startHr);
+    console.log("all sections: ", allSections);
+    // storing sections in a list for each day.  Will set the state as this, and render onto the DOM when there are values in there.
+    let monSections = [];
+    let tueSections = [];
+    let wedSections = [];
+    let thuSections = [];
+    let friSections = [];
+
+    for (let i = 0; i < allSections.length; i++) {
+      let currSection = allSections[i];
+      // the following code does a calculation to find the offset of the section square
+      // as well as the height based on how the calendar is structured.
+      let timeRanges = convertTimeStringTo24(currSection['hours']).split('-');
+      let sectionStart = moment(timeRanges[0], 'HH:mm');
+      let offset = ((sectionStart.diff(startHr, 'hours', true)) * 50) + 10; // 10 original top offset in px and 50 for height of each hr-row
+      console.log("LOOK HERE:");
+      console.log("time ranges: ", timeRanges);
+      console.log("start: ", sectionStart);
+      console.log("startHr: ", startHr);
+      console.log("OFFSET: ", offset);
+
+
+      let sectionEnd = moment(timeRanges[1], 'HH:mm');
+      let difference = sectionEnd.diff(sectionStart, 'hours', true);
+      let height = difference * 50; // 50px * height for event
+      console.log("height diff: ", difference);
+
+      console.log("time ranges: ", timeRanges); // if 1530 - 1620, expect 0.83333
+
+      // now that we have the offset and height, we can place into the calendar by inserting as a child of mon, tues, wedn, etc. column
+      let days = currSection['days'];
+
+      for (let j = 0; j < days.length; j++) {
+        let currChar = days.charAt(j);
+
+        // create new element with computed offset and height of section div.
+        let newElementEventStyle = {
+          display: 'block',
+          top: `${offset}px`,
+          backgroundColor: '#fff2bf',
+          borderLeft: '4px solid black',
+          height: `${height}px`,
+          width: '100%',
+          opacity: '0.6',
+          position: 'absolute',
+          zIndex: '1',
+        };
+
+        let newElementTextStyle = {
+          fontSize: '13px',
+          fontWeight: '500',
+          lineHeight: '14px',
+          padding: '4px 6px 4px 6px',
+          wordBreak: 'break-word'
+        }
+
+
+        let newElement = (
+          <div key={id++} style={newElementEventStyle}>
+            <div className="text">
+              <div className="title">
+                {currSection['course_id']}
+              </div>
+              <div className="location">
+                {currSection['location']}
+              </div>
+            </div>
+          </div>
+        );
+        switch (currChar) {
+          case 'M':
+            monSections.push(newElement);
+            break;
+          case 'T':
+            tueSections.push(newElement);
+            break;
+          case 'W':
+            wedSections.push(newElement);
+            break;
+          case 'R':
+            thuSections.push(newElement);
+            break;
+          case 'F':
+            friSections.push(newElement);
+            break;
+          default:
+            break;
+        }
+      }
+    }
+
+    this.setState(() => ({
+      mondaySections: monSections,
+      tuesdaySections: tueSections,
+      wednesdaySections: wedSections,
+      thursdaySections: thuSections,
+      fridaySections: friSections,
+      currIndex: indexUpdate,
+    }));
+
   };
 
   handleLeftArrowButton = () => {
-    const currIdx = this.state.currIndex;
+
+    let currIdx = this.state.currIndex;
+
     const numValidSchedules = this.props.validSchedules.length;
     if (currIdx == 0) {
-      this.setState(() => ({ currIndex: numValidSchedules-1 }));
-      return;
+      currIdx = numValidSchedules - 1;
     }
-    this.setState(() => ({ currIndex: currIdx - 1 }));
+    else {
+      currIdx -= 1;
+    }
+
+    // update event placement
+    const currSchedule = this.props.validSchedules[currIdx];
+    const sectionsList = extractSectionsFromSchedule(currSchedule);
+
+    const earliest = currSchedule['info']['earliest'];
+    const start = (Math.floor(earliest/100)*100)/100 - 1;
+
+    this.placeSectionsIntoCalendar(start, sectionsList, currIdx);
+
+
+
+
+
   };
 
   handleRightArrowButton = () => {
-    const currIdx = this.state.currIndex;
+
+    let currIdx = this.state.currIndex;
+
     const numValidSchedules = this.props.validSchedules.length;
     if (currIdx == numValidSchedules-1) {
-      this.setState(() => ({ currIndex: 0 }));
-      return;
+      currIdx = 0;
     }
-    this.setState(() => ({ currIndex: currIdx + 1 }));
+    else {
+      currIdx += 1;
+    }
+
+    const currSchedule = this.props.validSchedules[currIdx];
+    const sectionsList = extractSectionsFromSchedule(currSchedule);
+
+    const earliest = currSchedule['info']['earliest'];
+    const start = (Math.floor(earliest/100)*100)/100 - 1;
+
+
+
+    this.placeSectionsIntoCalendar(start, sectionsList, currIdx);
+
+
+
+
   };
 
   render() {
+    console.log("RENDERING IN SCHEDULES.JS");
     if (!this.props.validSchedules) {
       return <div className="schedules__container">Loading...</div>;
     }
+
     const currIdx = this.state.currIndex;
+    const currSchedule = this.props.validSchedules[currIdx];
     const numValidSchedules = this.props.validSchedules.length;
+
+    const earliest = currSchedule['info']['earliest'];
+    const latest = currSchedule['info']['latest'];
+    const start = (Math.floor(earliest/100)*100)/100 - 1;
+    const end = (Math.ceil(latest/100)*100)/100 + 1;
+
     return (
       <div className="schedules__container">
         <div className="schedules__buttons-container">
@@ -62,7 +246,17 @@ class Schedules extends React.Component {
         </div>
         <div className="calendar__top-header-divider"></div>
         <WeeklyCalendarHeader />
-        <WeeklyCalendarView currSchedule={this.props.validSchedules[currIdx]} />
+        <WeeklyCalendarView
+          monSections={this.state.mondaySections}
+          tueSections={this.state.tuesdaySections}
+          wedSections={this.state.wednesdaySections}
+          thuSections={this.state.thursdaySections}
+          friSections={this.state.fridaySections}
+          currSchedule={currSchedule}
+          parentIdx={currIdx}
+          startTime={start}
+          endTime={end}
+        />
 
       </div>
     );
--- src/components/WeeklyCalendarView.js
+++ src/components/WeeklyCalendarView.js
@@ -1,149 +1,9 @@
 import React from 'react';
-import moment from 'moment';
-import { extractSectionsFromSchedule } from './../lib/WeeklyCalendarUtils';
 import { convert24to12HourFormat } from './../lib/WeeklyCalendarUtils';
-import { convertTimeStringTo24 } from './../lib/WeeklyCalendarUtils';
 
 let id = 0;
 
 export default class WeeklyCalendarView extends React.Component {
-
-  state = {
-    currIndex: 0,
-    mondaySections: [],
-    tuesdaySections: [],
-    wednesdaySections: [],
-    thursdaySections: [],
-    fridaySections: [],
-  };
-
-  componentDidMount() {
-
-    // initial rendering of sections onto page
-    const currSchedule = this.props.currSchedule;
-    const sectionsList = extractSectionsFromSchedule(currSchedule);
-
-    const earliest = currSchedule['info']['earliest'];
-    const start = (Math.floor(earliest/100)*100)/100 - 1;
-
-    this.placeSectionsIntoCalendar(start, sectionsList);
-  }
-
-  placeSectionsIntoCalendar = (startingHour, allSections) => {
-    let startHr = (startingHour * 100).toString(10); // will be used for moment js
-
-    let hr;
-    if (startingHour < 10) {
-       hr = startHr.substring(0, 1);
-    }
-    else {
-      hr = `0${startHr.substring(0, 1)}`;
-    }
-    const min = startHr.slice(-2);
-    startHr = moment(`${hr}:${min}`, 'HH:mm');
-
-    console.log("starting hour: ", startHr);
-    console.log("all sections: ", allSections);
-    // storing sections in a list for each day.  Will set the state as this, and render onto the DOM when there are values in there.
-    let monSections = [];
-    let tueSections = [];
-    let wedSections = [];
-    let thuSections = [];
-    let friSections = [];
-
-    for (let i = 0; i < allSections.length; i++) {
-      let currSection = allSections[i];
-      // the following code does a calculation to find the offset of the section square
-      // as well as the height based on how the calendar is structured.
-      let timeRanges = convertTimeStringTo24(currSection['hours']).split('-');
-      let start = moment(timeRanges[0], 'HH:mm');
-      let offset = ((start.diff(startHr, 'hours', true)) * 50) + 10; // 10 original top offset in px and 50 for height of each hr-row
-      console.log("LOOK HERE:");
-      console.log("time ranges: ", timeRanges);
-      console.log("start: ", start);
-      console.log("startHr: ", startHr);
-      console.log("OFFSET: ", offset);
-
-
-      let end = moment(timeRanges[1], 'HH:mm');
-      let difference = end.diff(start, 'hours', true);
-      let height = difference * 50; // 50px * height for event
-      console.log("height diff: ", difference);
-
-      console.log("time ranges: ", timeRanges); // if 1530 - 1620, expect 0.83333
-
-      // now that we have the offset and height, we can place into the calendar by inserting as a child of mon, tues, wedn, etc. column
-      let days = currSection['days'];
-
-      for (let j = 0; j < days.length; j++) {
-        let currChar = days.charAt(j);
-
-        // create new element with computed offset and height of section div.
-        let newElementEventStyle = {
-          display: 'block',
-          top: `${offset}px`,
-          backgroundColor: '#fff2bf',
-          borderLeft: '4px solid black',
-          height: `${height}px`,
-          width: '100%',
-          opacity: '0.6',
-          position: 'absolute',
-          zIndex: '1',
-        };
-
-        let newElementTextStyle = {
-          fontSize: '13px',
-          fontWeight: '500',
-          lineHeight: '14px',
-          padding: '4px 6px 4px 6px',
-          wordBreak: 'break-word'
-        }
-
-
-        let newElement = (
-          <div key={id++} style={newElementEventStyle}>
-            <div className="text">
-              <div className="title">
-                {currSection['course_id']}
-              </div>
-              <div className="location">
-                {currSection['location']}
-              </div>
-            </div>
-          </div>
-        );
-        switch (currChar) {
-          case 'M':
-            monSections.push(newElement);
-            break;
-          case 'T':
-            tueSections.push(newElement);
-            break;
-          case 'W':
-            wedSections.push(newElement);
-            break;
-          case 'R':
-            thuSections.push(newElement);
-            break;
-          case 'F':
-            friSections.push(newElement);
-            break;
-          default:
-            break;
-        }
-      }
-
-    }
-
-    this.setState(() => ({
-      mondaySections: monSections,
-      tuesdaySections: tueSections,
-      wednesdaySections: wedSections,
-      thursdaySections: thuSections,
-      fridaySections: friSections,
-    }));
-
-  };
 
   renderWeekColumnRows = (numRowsToRender) => {
     let times = [];
@@ -163,7 +23,6 @@ export default class WeeklyCalendarView extends React.Component {
   };
 
   renderTimes = (start, end) => {
-    console.log(`Earliest time: ${start}, Latest time: ${end}`);
 
     let times = [];
     for (let i = start; i <= end; i++) {
@@ -187,27 +46,8 @@ export default class WeeklyCalendarView extends React.Component {
     );
   };
 
-  renderSections = (day) => {
+  renderSections = (sections) => {
     // return array of divs based on day
-    let sections = [];
-
-    switch (day) {
-      case 'mon':
-        sections = this.state.mondaySections;
-        break;
-      case 'tue':
-        sections = this.state.tuesdaySections;
-        break;
-      case 'wed':
-        sections = this.state.wednesdaySections;
-        break;
-      case 'thu':
-        sections = this.state.thursdaySections;
-        break;
-      case 'fri':
-        sections = this.state.fridaySections;
-        break;
-    }
 
     // if empty, return invisible div.
     if (sections.length === 0) {
@@ -222,16 +62,21 @@ export default class WeeklyCalendarView extends React.Component {
   };
 
   render() {
-    console.log("[WEEKLY VIEW] current valid schedule: ", this.props.currSchedule);
-    console.log("[weekly view] STATE: ", this.state);
-    const earliest = this.props.currSchedule['info']['earliest'];
-    const latest = this.props.currSchedule['info']['latest'];
-    const start = (Math.floor(earliest/100)*100)/100 - 1;
-    const end = (Math.ceil(latest/100)*100)/100 + 1;
+    // console.log("[WEEKLY VIEW] current valid schedule: ", this.props.currSchedule);
+    // console.log("[weekly view] STATE: ", this.state);
+    const startTime = this.props.startTime;
+    const endTime = this.props.endTime;
+
+    const monSections = this.props.monSections;
+    const tueSections = this.props.tueSections;
+    const wedSections = this.props.wedSections;
+    const thuSections = this.props.thuSections;
+    const friSections = this.props.friSections;
+
     return (
       <div className="weekly-cal-view__container">
         <div className="weekly-cal-view__time-col">
-          { this.renderTimes(start, end) }
+          { this.renderTimes(startTime, endTime) }
         </div>
         <div className="weekly-cal-view__monday-col" id="monday">
           {/* <div className="event">
@@ -244,25 +89,25 @@ export default class WeeklyCalendarView extends React.Component {
               </div>
             </div>
           </div> */}
-          { this.renderWeekColumnRows(end-start) }
-          { this.renderSections('mon') }
+          { this.renderWeekColumnRows(endTime-startTime) }
+          { this.renderSections(monSections) }
 
         </div>
         <div className="weekly-cal-view__tuesday-col" id="tuesday">
-          { this.renderWeekColumnRows(end-start) }
-          { this.renderSections('tue') }
+          { this.renderWeekColumnRows(endTime-startTime) }
+          { this.renderSections(tueSections) }
         </div>
         <div className="weekly-cal-view__wednesday-col" id="wednesday">
-          { this.renderWeekColumnRows(end-start) }
-          { this.renderSections('wed') }
+          { this.renderWeekColumnRows(endTime-startTime) }
+          { this.renderSections(wedSections) }
         </div>
         <div className="weekly-cal-view__thursday-col" id="thursday">
-          { this.renderWeekColumnRows(end-start) }
-          { this.renderSections('thu') }
+          { this.renderWeekColumnRows(endTime-startTime) }
+          { this.renderSections(thuSections) }
         </div>
         <div className="weekly-cal-view__friday-col" id="friday">
-          { this.renderWeekColumnRows(end-start) }
-          { this.renderSections('fri') }
+          { this.renderWeekColumnRows(endTime-startTime) }
+          { this.renderSections(friSections) }
         </div>
       </div>
     );
