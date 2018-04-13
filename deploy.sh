70eb42567d2ac977af8f0bd85851d2716b8afb92
Miguel Hernandez <mhernandez401405@gmail.com>
2018-04-12 21:47:01 -0700

    script should work now

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
+
 git branch -D gh-pages
 git checkout -b gh-pages
+yarn install
 yarn run build:prod
 shopt -s extglob  # enables extglob
 rm -rf !(deploy|public)
 cp -r public/* $PWD
-git commit -am 'update deploy build'
+rm -r public/
+git add .
+git commit -m 'update deploy build'
 git push origin gh-pages
 git checkout master
 shopt -u extglob  # disables extglob
