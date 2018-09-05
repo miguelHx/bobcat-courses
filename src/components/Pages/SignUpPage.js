1650ef53a20b38ac2b2aef1d2e7ca92b3345404b
miguelhx <mhernandez401405@gmail.com>
2018-09-05 14:29:56 -0700

    better error handling in sign up page

--- src/components/Pages/SignUpPage.js
+++ src/components/Pages/SignUpPage.js
@@ -100,8 +100,20 @@ class SignUpPage extends React.Component {
       }
     })
     .catch(error => {
-      // console.log(error);
-      this.setState(() => ({ error: error }));
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        this.setState(() => ({ error: error.response.data['error'] }));
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+      }
+      // console.log(error.config);
     });
   };
 
