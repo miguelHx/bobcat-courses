786d03c6886adc91958eae2e2a050e123569e46a
miguelhx <mhernandez401405@gmail.com>
2018-09-05 14:09:53 -0700

    Import needed semantic ui css files only

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
 import registerServiceWorker from './registerServiceWorker';
+import './semantic-ui-css-imports';
 import './index.css';
-import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(<AppRouter />, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/semantic-ui-css-imports.js
@@ -0,0 +1,33 @@
+
+// app.js
+import 'semantic-ui-css/components/message.min.css';
+import 'semantic-ui-css/components/button.min.css';
+
+// AddCourse.js
+import 'semantic-ui-css/components/dropdown.min.css';
+import 'semantic-ui-css/components/site.min.css';
+import 'semantic-ui-css/components/reset.min.css';
+import 'semantic-ui-css/components/transition.min.css';
+import 'semantic-ui-css/components/menu.min.css';
+
+
+// semantic-ui-css imports:
+// import 'semantic-ui-css/components/icon.css';
+
+// SectionsTable.js
+import 'semantic-ui-css/components/table.min.css';
+import 'semantic-ui-css/components/checkbox.min.css';
+
+
+// Login Form
+import 'semantic-ui-css/components/form.min.css';
+import 'semantic-ui-css/components/segment.min.css';
+
+// Popup
+import 'semantic-ui-css/components/popup.min.css';
+
+// Loader
+import 'semantic-ui-css/components/loader.min.css';
+
+// Sidebar
+import 'semantic-ui-css/components/sidebar.min.css';
