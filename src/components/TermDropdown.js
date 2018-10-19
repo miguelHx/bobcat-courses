c2d729d0a4fc66f6214b9ad2cf2a7f9558a970c1
miguelhx <mhernandez401405@gmail.com>
2018-10-19 12:24:16 -0700

    more UI improvements, remove spring 2018 term

--- src/components/TermDropdown.js
+++ src/components/TermDropdown.js
@@ -8,7 +8,6 @@ import { setSelectedTerm } from "../react-redux/actions/selectedTerm";
 const TERM_OPTIONS = [
   { key: 'Spring 2019', value: '201910', text: 'Spring 2019' },
   { key: 'Fall 2018', value: '201830', text: 'Fall 2018' },
-  { key: 'Spring 2018', value: '201810', text: 'Spring 2018' },
 ];
 
 
